2cc1460f36e14b4a0b0d9d587079ed1a78d0327e
Denis Popov <dpopov@saber.games>
2024-01-18 17:25:32 +0300

    {% url %} added (not working)

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'other',
     'login',
     'store',
+
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path # , include
+from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('grappelli/', admin.site.urls),  # grapelli URLS
+    path('admin/', admin.site.urls),  # admin site
     path('other/', include('other.urls')),
     path('login/', include('login.urls')),
     path('store/', include('store.urls')),
-
 ]
+
+
